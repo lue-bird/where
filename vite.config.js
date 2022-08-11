7019f9c1242abcca72a9f7cdb3427bfc5ab38f36
lue-bird <lue.the.bird@gmail.com>
2022-08-11 21:37:11 +0200

    config base +

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
 
 export default defineConfig({
+  base: "/dice-digging/",
   plugins: [elmPlugin({ debug: false, optimize: false })],
 });
